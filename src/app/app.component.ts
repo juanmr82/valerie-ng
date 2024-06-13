3d18139868dfb500ff88d630f06c5bcc49be67e0
Juan Reina <juanmr82@gmail.com>
2024-06-13 19:04:12 +0200

    First Commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {CalendarComponent} from "./calendar/calendar.component";
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
+  imports: [RouterOutlet,CalendarComponent],
+  template: '<app-calendar></app-calendar>',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'valerie-ng';
+  title = 'frontend';
 }
